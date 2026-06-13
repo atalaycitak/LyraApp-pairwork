87f180ea5fdf6ffda6e4426c78f75d47a90b93e2
atalaycitak <atalaycitak@gmail.com>
2026-06-13 10:30:24 +0300

    chore: update compileSdk to 36 and initialize Android Studio project configuration files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,7 @@ plugins {
 
 android {
     namespace = "com.turkcell.lyraapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.turkcell.lyraapp"
